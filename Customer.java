6de14e23af69290edd7f65a51631b1e19ab93a29
Galih Prayoga <galihp83@gmail.com>
2024-06-02 00:51:06 +0700

    Create custommer class and attributes

---
+++ Customer.java
@@ -0,0 +1,29 @@
+public class Customer{
+    private String nama;
+    private String alamat;
+    private String ewallet;
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public void setAlamat(String alamat){
+        this.alamat = alamat;
+    }
+
+    public String getAlamat(){
+        return alamat;
+    }
+
+    public void setEwallet(String ewallet){
+        this.ewallet = ewallet;
+    }
+
+    public String getEwallet(){
+        return ewallet;
+    }
+}
