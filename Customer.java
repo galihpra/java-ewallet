5108fdb713f3c000fa0ef3c34f42e02ad793bfab
Galih Prayoga <galihp83@gmail.com>
2024-06-02 01:34:09 +0700

    Adding Create method customer

--- Customer.java
+++ Customer.java
@@ -1,6 +1,8 @@
-public class Customer{
+import java.util.Scanner;
+
+class CustomerData{
     private String nama;
-    private String alamat;
+    String alamat;
     private String ewallet;
 
     public void setNama(String nama){
@@ -11,19 +13,26 @@ public class Customer{
         return nama;
     }
 
-    public void setAlamat(String alamat){
-        this.alamat = alamat;
-    }
-
-    public String getAlamat(){
-        return alamat;
-    }
-
     public void setEwallet(String ewallet){
         this.ewallet = ewallet;
     }
 
     public String getEwallet(){
         return ewallet;
+    }
+}
+
+public class Customer {
+    public static void main(String[] args) {
+        CustomerData signUp = new CustomerData();
+
+        signUp.setNama("Galih Prayoga");
+        System.out.println("Anda terdaftar sebagai "+signUp.getNama());
+        
+        signUp.alamat = "Wonogiri";
+        System.out.println("Alamat yang anda daftarkan adalah "+signUp.alamat);
+
+        signUp.setEwallet("OVO");
+        System.out.println("E-wallet yang anda pilih adalah "+signUp.getEwallet());
     }
 }
